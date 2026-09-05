d7a69be11dd074155f649b23c6ffcce381ca5aed
tarun2195 <tarun172342@gmail.com>
2026-09-05 21:42:54 +0530

    adding api handlers

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -46,15 +46,92 @@ export const createEmployee = async (req, res) => {
 // GET ALL EMPLOYEES
 export const getEmployees = async (req, res) => {
     try {
-        const result = await connection.query(
-            `SELECT * FROM employee
-             ORDER BY hired_at DESC`
-        );
+         const {
+      page = 1,
+      limit = 10,
+      search,
+      department,
+      sortBy= "hired_at",
+      order= "desc"
+    } = req.query;
 
-        res.status(200).json({
-            success: true,
-            data: result.rows
-        });
+    // convert to numbers
+    const pageNum = parseInt(page, 10);
+    const limitNum = parseInt(limit, 10);
+
+    // calculate offset
+    const offset = (pageNum - 1) * limitNum;
+
+     // allow only specific columns for sorting
+    const allowedSortFields = ["salary", "hired_at"];
+
+    const sortColumn = allowedSortFields.includes(sortBy)
+      ? sortBy
+      : "hired_at";
+
+    // allow only ASC or DESC
+    const sortOrder = order.toLowerCase() === "asc"
+      ? "ASC"
+      : "DESC";
+
+    let result;
+
+    if (search && department) {
+      // prepare search value with wildcards
+      const searchValue = `%${search}%`;
+
+      result = await connection.query(
+        `SELECT * FROM employee
+         WHERE (first_name ILIKE $1
+            OR last_name ILIKE $1
+            OR email_id ILIKE $1)
+            AND department = $2
+         ORDER BY hired_at DESC
+         LIMIT $3 OFFSET $4`,
+        [searchValue, department, limitNum, offset]
+      );
+
+    } else if (search) {
+      // search only
+      const searchValue = `%${search}%`;
+
+      result = await connection.query(
+        `SELECT * FROM employee
+         WHERE first_name ILIKE $1
+            OR last_name ILIKE $1
+            OR email_id ILIKE $1
+         ORDER BY hired_at DESC
+         LIMIT $2 OFFSET $3`,
+        [searchValue, limitNum, offset]
+      );
+
+    } else if (department) {
+      // department filter only
+      result = await connection.query(
+        `SELECT * FROM employee
+         WHERE department = $1
+         ORDER BY hired_at DESC
+         LIMIT $2 OFFSET $3`,
+        [department, limitNum, offset]
+      );
+
+    } else {
+      // no search and no department filter
+      result = await connection.query(
+        `SELECT * FROM employee
+         ORDER BY hired_at DESC
+         LIMIT $1 OFFSET $2`,
+        [limitNum, offset]
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      data: result.rows,
+      page: pageNum,
+      limit: limitNum
+    });
+
 
     } catch (error) {
         console.error(error);
@@ -122,7 +199,7 @@ export const updateEmployee = async (req, res) => {
                 email_id = $3,
                 department = $4,
                 salary = $5,
-                is_active = $6,
+                is_active = $6
                 updated_at = CURRENT_TIMESTAMP
              WHERE emp_id = $7
              RETURNING *`,
